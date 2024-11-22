6e2a94b12195c79ec3be85beeac2a2b592e63773
TobiasEwert <151402113+TobiasEwert@users.noreply.github.com>
2024-11-22 18:47:55 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,75 +1,56 @@
 import socket
 import threading
+from ClientHandler import ClientHandle
+import ast
 
-def client_commands(conn, address): # used to connect to a single client
-        print("Connection from: ", str(address)) # tells who is connecting (Ip address)
-        while True:
-            data = conn.recv(1024).decode() # limits amound of data by 1kb
-            if not data: # if no data then break
-                break
-            print("Response from:", str(data))
-            data = ' : ' + commands(data)
-            conn.send(data.encode()) # send data
 
-        conn.close()
-        print("Connection closed")
 
 
 def server_program():
-    # get the hostname
     host = socket.gethostname()
-    port = 5000  # initiate port no above 1024
+    port = 5000  # Port to bind the server
 
-    server_socket = socket.socket()  # get instance
-    # look closely. The bind() function takes tuple as argument
-    server_socket.bind((host, port))  # bind host address and port together
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((host, port))
+    server_socket.listen(4)
+    print("Server is listening on port", port)
 
-    # configure how many client the server can listen simultaneously
-    server_socket.listen(2)
-    print(f"Server started on {host}:{port}") # added to let us know the server has started up
+    # Input thread for server console, ensures when calling input it does not lock up the main thread
+    input_thread = threading.Thread(target=Console)
+    input_thread.start()
+
+    try:
+        open("Users.txt", 'x')
+    except FileExistsError:
+        pass
+
+    with open("Users.txt", 'r') as file:
+        data = file.read()
+
+        if data.strip():    # If the file is not empty, grab all users from the file
+            ClientHandle.SetUserDict(ast.literal_eval(data))
+
+        else:   # If empty, set as an empty dictionary
+            ClientHandle.SetUserDict(dict([]))
+
+    ClientHandle.SetRSA((host, 4000))
+
 
     while True:
+        # Accept new connection
         conn, address = server_socket.accept()
-        # used to create a thread for every new client
-        client_thread = threading.Thread(target=client_commands, args=(conn, address))
+
+        # Start a new thread to handle each client
+        client_thread = threading.Thread(target=ClientHandle(conn, address).handle_client)
         client_thread.start()
 
-    #conn, address = server_socket.accept()  # accept new connection
-    #print("Connection from: " + str(address))
-    #while True:
-        # receive data stream. it won't accept data packet greater than 1024 bytes
-        #data = conn.recv(1024).decode()
-        #if not data:
-            # if data is not received break
-            #break
-        #print("from connected user: " + str(data))
-        #data = ' -> ' + commands(data)
-        #conn.send(data.encode())  # send data to the client
 
-    #conn.close()  # close the connection
 
-def commands(request):
-    match request:
-        case "Connect":
-            return "Initiates connection from client to server with the specified files"
-
-        case "Upload":
-            return "Clients can upload files to the server. Server will prompt client if file already exists and requires user input to be overwritten"
-
-        case "Download":
-            return "Clients can download files from the server. Server will respond with error message if file does not exist."
-
-        case "Delete":
-            return "Clients can delete files from the server. Server will respond with error message if file is currently being processed or does not exist."
-
-        case "Dir":
-            return "Clients can view a list of files and subdirectories in the server’s file storage path."
-
-        case "Subfolder":
-            return "Clients can create or subfolders in the server’s file storage path"
-
-        case _:
-            return "I did not understand that command, please try again"
+# Function for input thread to use
+def Console():
+    command = ""
+    while command.lower().strip() != 'shutdown':
+        command = input("server/ ")
 
 
 # Press the green button in the gutter to run the script.
